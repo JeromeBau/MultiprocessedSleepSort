5c32fda622115b5cb71d0eb3a72d62d343c333b7
Jerome Bau <accounts@jerome-bau.de>
2017-06-26 12:54:08 +0200

    cleaned code, wrote readme

--- sleep_sort.py
+++ sleep_sort.py
@@ -1,24 +1,25 @@
 import multiprocessing as mp
 import time
 
+results = []
 
-to_be_sorted = [7, 3, 65, 1, 43, 908, 2, 98]
 
 def sort_one_number(x, speed):
     """ Function to be executed by the pool workers"""
     time.sleep(x*speed)
-    print(x)
     return x
 
-results = []
+
 def save_results(result):
     results.append(result)
 
-def start_sorting(speed = 0.001):
+
+def start_sorting(to_be_sorted, speed=0.001):
     pool = mp.Pool()
     for i in to_be_sorted:
-        pool.apply_async(sort_one_number, args = (i, speed, ), callback = save_results)
+        pool.apply_async(sort_one_number, args=(i, speed, ), callback=save_results)
     pool.close()
     pool.join()
     return results
 
+start_sorting(speed=0.1, to_be_sorted=[7, 3, 65, 2, 98])
